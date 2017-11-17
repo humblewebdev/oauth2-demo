a73b4518a510d4126be5a654c3e0b7ef621457e1
Thomas Veilleux <tom@humblewebdev.com>
2017-11-16 21:28:07 -0600

    added controller

--- src/main/java/com/humblewebdev/oauth2demo/controller/OAuth2ApiController.java
+++ src/main/java/com/humblewebdev/oauth2demo/controller/OAuth2ApiController.java
@@ -1,4 +1,10 @@
 package com.humblewebdev.oauth2demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api");
 public class OAuth2ApiController {
+    public static final Logger logger = LoggingFactory.getLogger(OAuth2ApiController.class);
 }
